62510ff5926f7e93c6ff49b27559adad431bff9d
Jorge Abad <jorgejoseabad@gmail.com>
2017-06-01 12:48:56 +0200

    5 iteration with better code

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,4 +1,37 @@
 
+function ploter(x,y){
+    var ctx = document.getElementById("myLineChart");
+      var myLineChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: x,
+        datasets: [{
+              label: "valormodular",
+              fill: false,
+               lineTension: 0.1,
+               backgroundColor: "rgba(75,192,192,0.4)",
+               borderColor: "rgba(75,192,192,1)",
+               borderCapStyle: 'butt',
+               borderDash: [],
+               borderDashOffset: 0.0,
+               borderJoinStyle: 'miter',
+               pointBorderColor: "rgba(75,192,192,1)",
+               pointBackgroundColor: "#fff",
+               pointBorderWidth: 1,
+               pointHoverRadius: 5,
+               pointHoverBackgroundColor: "rgba(75,192,192,1)",
+               pointHoverBorderColor: "rgba(220,220,220,1)",
+               pointHoverBorderWidth: 2,
+               pointRadius: 1,
+               pointHitRadius: 10,
+              data: y,
+              spanGaps: false,
+
+          }],
+        }
+      });
+    }
+
 function writeMaxMin(x,y){
   var min = Math.min.apply(0,y);
   var max = Math.max.apply(0,y);
@@ -6,6 +39,23 @@ function writeMaxMin(x,y){
   document.getElementById("max").value = max;
   document.getElementById("min").value= min;
 }
+
+function workData(response){
+  var obj = jQuery.parseJSON(response);
+  console.log(obj);
+  console.log(obj.bpi);
+  var data=obj.bpi;
+  var x = Object.keys(obj.bpi);
+  var y = Object.values(obj.bpi);
+  //console.log(data);
+  console.log(x);
+  console.log(y);
+  ploter(x,y);
+  writeMaxMin(x,y);
+  console.log("respuesta recogida");
+  return data;
+}
+
 
 $("#currency").change(function(){
   var currency = document.getElementById("currency").value;
@@ -18,22 +68,8 @@ function getFinancialDatabyCurrency(currency) {
     url: "http://api.coindesk.com/v1/bpi/historical/close.json?currency="+currency,
     method: "GET",
     success: function (response) {
+      workData(response);
 
-      console.log(response);
-      var obj = jQuery.parseJSON(response);
-      console.log(obj);
-      console.log(obj.bpi);
-      var data=obj.bpi;
-      var x = Object.keys(obj.bpi);
-      var y = Object.values(obj.bpi);
-      console.log(data);
-      console.log(x);
-      console.log(y);
-      ploter(x,y);
-      writeMaxMin(x,y);
-
-      console.log("respuesta recogida");
-      return data;
     },
     error: function (err) {
       console.log(err);
@@ -48,23 +84,8 @@ function getFinInfo(id) {
     url: "http://api.coindesk.com/v1/bpi/historical/close.json",
     method: "GET",
     success: function (response) {
+      workData(response);
 
-      console.log(response);
-      var obj = jQuery.parseJSON(response);
-      console.log(obj);
-      console.log(obj.bpi);
-      var data=obj.bpi;
-      var x = Object.keys(obj.bpi);
-      var y = Object.values(obj.bpi);
-      console.log(data);
-      console.log(x);
-      console.log(y);
-      ploter(x,y);
-      writeMaxMin(x,y);
-
-
-      console.log("respuesta recogida");
-      return data;
     },
     error: function (err) {
       console.log(err);
@@ -86,8 +107,8 @@ $('#dates').click(function(){
 
   console.log(dataInit,dataEnd);
 
-  getFinancialDatabyDate(dataInit,dataEnd);
-
+  const data=getFinancialDatabyDate(dataInit,dataEnd);
+  console.log(data);
 });
 
 document.getElementById('start-date').addEventListener('click',function(){
@@ -97,7 +118,8 @@ document.getElementById('start-date').addEventListener('click',function(){
   console.log('on event listener start-date');
   console.log(dataInit,dataEnd);
 
-  getFinancialDatabyDate(dataInit,dataEnd);
+  const data=getFinancialDatabyDate(dataInit,dataEnd);
+  console.log(data);
 });
 
 document.getElementById('end-date').addEventListener('click',function(){
@@ -107,7 +129,8 @@ document.getElementById('end-date').addEventListener('click',function(){
   console.log('on event listener start-date');
   console.log(dataInit,dataEnd);
 
-  getFinancialDatabyDate(dataInit,dataEnd);
+  const data=getFinancialDatabyDate(dataInit,dataEnd);
+  console.log(data);
 });
 
 function getFinancialDatabyDate(dataInit,dataEnd){
@@ -117,21 +140,8 @@ function getFinancialDatabyDate(dataInit,dataEnd){
     //dataType: 'JSON',
     success: function(response){
       console.log(response);
+      workData(response);
 
-      var obj = jQuery.parseJSON(response);
-      console.log(obj);
-      console.log(obj.bpi);
-      var data=obj.bpi;
-      var x = Object.keys(obj.bpi);
-      var y = Object.values(obj.bpi);
-      //console.log(data);
-      console.log(x);
-      console.log(y);
-      ploter(x,y);
-      writeMaxMin(x,y);
-      console.log("respuesta recogida");
-      return data;
-      //return response.bpi;
       },
       error: function (err) {
       console.log(err);
